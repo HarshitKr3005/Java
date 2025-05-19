800ecb30903fb3c1b1c7bb2b1f28aba63dd954d8
Harshit Kumar <101578356+HarshitKr3005@users.noreply.github.com>
2025-05-19 13:27:55 +0530

    Create For_Loops.java

---
+++ For_Loops.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Code {
+    public static void main(String[] args){
+
+        /*
+        FOR LOOPS:--
+        for (int i=0;i<5;i++){
+            System.out.println("hi");
+
+        WHILE LOOPS:--
+        String input = "";
+        Scanner scanner = new Scanner(System.in);
+        while(!input.equals("quit")){
+            System.out.print("Input: ");
+            input = scanner.next().toLowerCase();
+            System.out.println(input);
+        }
+
+        DO WHILE LOOPS:--
+        String input = "";
+        Scanner scanner = new Scanner(System.in);
+        do{
+            System.out.print("Input: ");
+            input = scanner.next().toLowerCase();
+            System.out.println(input);
+        }while(!input.equals("quit"));
+
+        FOR EACH LOOP:--
+        String[] fruits = {"Apple", "Mango", "Orange"};
+        for (String s : fruits) {
+            System.out.println(s);
+        }
+        for(String fruit : fruits) {
+            System.out.println(fruit);
+        }
+
+         */
+    }
+}
