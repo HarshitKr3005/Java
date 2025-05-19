eec75c2afbc4919b5ff56308220edf41d35d8817
Harshit Kumar <101578356+HarshitKr3005@users.noreply.github.com>
2025-05-19 14:37:52 +0530

    Create Main.java

---
+++ Collections/Generic/Main.java
@@ -0,0 +1,13 @@
+package generic;
+
+import generic.GenericList;
+
+public class Main {
+    public static void main(String[] args) {
+        var list = new GenericList<String>();
+        list.add("a");
+        list.add("b");
+        for (var item : list)
+            System.out.println(item);
+    }
+}
