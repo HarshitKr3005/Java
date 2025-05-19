5557a54700571333e485d73052b3a0c34936b344
Harshit Kumar <101578356+HarshitKr3005@users.noreply.github.com>
2025-05-19 13:26:45 +0530

    Create Basics.java

---
+++ Basics.java
@@ -0,0 +1,91 @@
+import java.awt.*;
+import java.text.NumberFormat;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        /*
+        PRIMITIVE TYPES:--
+        long viewsCount = 3_123_456_789L;
+        float price = 10.99F;
+        char letter = 'A';
+        boolean isEligible = true;
+        byte age = 30;
+
+        REFERENCE TYPES:--
+        Date now = new Date();
+        System.out.println(now);
+
+        Point point1 = new Point(1, 1);
+        Point point2 = point1;
+        point1.x = 2;
+        System.out.println(point2);
+
+        STRINGS:--
+        String message = " Hello World" + "!! ";
+        System.out.println(message.length());
+        System.out.println(message.endsWith("4"));
+        System.out.println(message.startsWith("!!"));
+        System.out.println(message.indexOf("sky"));
+        System.out.println(message.replace("Hello", "Hi"));
+        System.out.println(message);
+        System.out.println(message.toLowerCase());
+        System.out.println(message.trim());
+        System.out.println(message);
+        String message = "c:\tWindows\\..";
+        System.out.println(message);
+
+        ARRAYS:--
+        int[] numbers = {2,3,5,1,4,6};
+        Arrays.sort(numbers);
+        System.out.println(Arrays.toString(numbers));
+
+        MULTI-DIMENSIONAL ARRAYS:--
+        int[][] numbers = {{1,2,3}, {4,5,6}};
+        System.out.println(Arrays.deepToString(numbers));
+
+        CONSTANT:--
+        final float PI = 3.14F;
+
+        ARITHMETIC OPERATIONS:--
+        double result = (double)10 / (double)3;
+        int x = 1;
+        x += 2;
+        System.out.println(x);
+
+        ORDER OF OPERATIONS:--
+        int x = 10+3*2;
+        System.out.println(x);
+
+        CASTING:--
+        String x = "1.1";
+        double y = Double.parseDouble(x) + 2;
+        System.out.println(y);
+
+        MATH FUNCTIONS:--
+        int res = Math.round(2.1F);
+        int res2 = (int)Math.ceil(2.4F);
+        int res3 = (int)Math.floor(2.2F);
+        int res4 = Math.max(1,2);
+        int res5 = (int)(Math.random() * 100);
+        System.out.println(res);
+        System.out.println(res2);
+        System.out.println(res3);
+        System.out.println(res4);
+        System.out.println(res5);
+
+        FORMATTING NUMBERS:--
+        NumberFormat currency = NumberFormat.getCurrencyInstance();
+        String res = currency.format(1234567.891);
+        System.out.println(res);
+
+        READING INPUT:--
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Name: ");
+        String name = scanner.nextLine().trim();
+        System.out.println("You are " + name + ".");
+        */
+    }
+}
