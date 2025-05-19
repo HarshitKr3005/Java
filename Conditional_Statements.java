7caf136f162e3b663c61401f80819fb0d176e120
Harshit Kumar <101578356+HarshitKr3005@users.noreply.github.com>
2025-05-19 13:27:24 +0530

    Create Conditional_Statements.java

---
+++ Conditional_Statements.java
@@ -0,0 +1,56 @@
+public class Code {
+    public static void main(String[] args) {
+
+        /*
+        COMPARISON OPERATORS:--
+        int x = 1;
+        int y = 1;
+        System.out.println(x == y);
+        System.out.println(x != y);
+        System.out.println(x < y);
+
+        LOGICAL OPERATORS:--
+        int temp = 22;
+        boolean isWarm = temp > 20 && temp < 30;
+        System.out.println(isWarm);
+
+        boolean hasHighIncome = false;
+        boolean hasGoodCredit = true;
+        boolean hasCriminalRecord = false;
+        boolean isEligible = (hasHighIncome || hasGoodCredit) && !hasCriminalRecord;
+
+        CONDITIONAL STATEMENTS:--
+        int temp = 32;
+        if (temp > 30) {
+            System.out.println("It's a hot day");
+            System.out.println("Drink water");
+        } else if (temp > 20)
+            System.out.println("Beautiful day");
+        else
+            System.out.println("Cold day");
+
+        int income = 120_000;
+        boolean hasHighIncome = (income > 100_000);
+
+        TERNARY OPERATORS:--
+        int income = 120_000;
+        String className = income > 100_000 ? "First" : "Economy";
+
+        SWITCH STATEMENT:--
+        String role = "admin";
+        switch (role) {
+            case "admin":
+                System.out.println("You are an admin.");
+                break;
+
+            case "moderator":
+                System.out.println("You are a moderator");
+                break;
+
+            default:
+                System.out.println("You are a guest");
+        }
+
+         */
+    }
+}
